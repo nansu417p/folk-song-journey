a58fc66572044cb22d6bbdcd420782cf7399bd3c
nansu417p <nansu417p@gmail.com>
2025-12-04 00:54:01 +0800

    12/4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 這是讓內網可以連線
+    // ★★★ 新增底下這一行 ★★★
+    allowedHosts: true, 
+  }
 })
